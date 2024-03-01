cc8c36fb40729a7fbd913cb87b69566a1c526555
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-03-01 22:37:18 +0900

    仮のログイン機能実装

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -5,7 +5,14 @@ type LoginPayload = {
 };
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function login(payload: LoginPayload) {
-  return { success: !true };
+  return {
+    success:
+      [
+        "admin@example.com",
+        "editor@example.com",
+        "viewer@example.com",
+      ].includes(payload.email) && payload.password === "password",
+  };
 }
 
 export async function isLoggedIn() {
