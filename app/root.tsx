6c85e0585628ebd19d3e9415a2c254576995ac15
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-03-03 23:35:12 +0900

    ログイン画面がスクロールできない不具合修正

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="flex flex-col h-dvh overflow-hidden">
+      <body className="flex flex-col h-dvh">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
