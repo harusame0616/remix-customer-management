fbb8ddd5541195dc4dc5ed17aa49092f53a0f8f9
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-03-07 19:50:29 +0900

    seed で投入前にリセット

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,11 @@ import prisma from "~/lib/prisma";
 
 async function main() {
   await Promise.all([
+    prisma.dealStatus.deleteMany(),
+    prisma.dealPlatform.deleteMany(),
+  ]);
+
+  await Promise.all([
     prisma.dealStatus.createMany({
       data: Object.values(DealStatus),
     }),
